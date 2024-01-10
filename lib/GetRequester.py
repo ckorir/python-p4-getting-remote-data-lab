b86f796871724449fc44a251cd347139a22f8741
ckorir <chrisruto.k@gmail.com>
2024-01-10 11:16:15 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
+
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+        json_content = json.loads(response_body)  # Use json.loads to parse the JSON content
+        return json_content
         pass
